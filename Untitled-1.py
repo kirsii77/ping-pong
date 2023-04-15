4e9b51ac6810dbc6a1fbf98c443f17957be2718e
kirsii77 <129597891+kirsii77@users.noreply.github.com>
2023-04-15 15:13:25 +0300

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -29,21 +29,40 @@ class Player(GameSprite):
             
 raketka1 = Player('raketka.jpg', 20, 400, 20, 100, 10)
 raketka2 = Player('raketka.jpg', 970, 400, 20, 100, 10)
+ball = GameSprite('ball (2).png', 500, 300, 80, 70, 5)
 window = display.set_mode((win_width, win_height))
 display.set_caption('пинг понг')
 background = transform.scale(image.load('background.jpg'), (win_width, win_height))
 speed = 10
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 game = True
 clock = time.Clock()
 FPS = 60
+speed_x = 5
+speed_y = 5
 while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
     window.blit(background, (0,0))
     raketka1.reset()
     raketka1.update_a()
     raketka2.reset()
     raketka2.update_b()
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
+    ball.reset()
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+    if ball.rect.y >= 580:
+        ball.rect.y *= -1
+    elif ball.rect.y < 1:
+        ball.rect.y *= -1
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+    if ball.rect.x > 1000:
+        finish = True
+        window.blit(lose2, (200, 200))
     display.update()
     clock.tick(FPS)
